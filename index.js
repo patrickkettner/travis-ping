e970c2f2dd660e450159d7bea146c35827ba85d4
Arnold Daniels <arnold@jasny.net>
2015-07-26 14:57:11 +0200

    Added option to filter on branch and/or event type Filter on branch by name Filter on event type (push or pull-request) Upgrade to travis-ci package v2.x (from v1.x) Show build number in output when successfull Show proper error when no builds are found
    !!BC Break - Major changes!!

--- index.js
+++ index.js
@@ -16,7 +16,27 @@ var getCredentials = function (cb) {
   }], cb);
 };
 
-var travisPing = function(credentials, repo, cb) {
+var findBuild = function(travis, builds, commits, filter, cb) {
+  var commitIndex = {};
+  commits.forEach(function(commit) {
+    commitIndex[commit.id] = commit;
+  });
+
+  for (var i = 0; i < builds.length; i++) {
+    var build = builds[i];
+    var commit = commitIndex[build.commit_id];
+  
+    if (filter.eventType && build.event_type !== filter.eventType) continue;
+    if (filter.branch && commit.branch !== filter.branch) continue;
+    
+    return cb(null, build);
+  }
+  
+  var type = (filter.eventType ? filter.eventType.replace('_', ' ') + ' ' : '');
+  return cb('no ' + type + 'builds found for this ' + (filter.branch ? 'branch' : 'repo'));
+};
+
+var travisPing = function(credentials, repo, filter, cb) {
   var travis = new Travis({
     version: '2.0.0',
     pro: credentials.pro
@@ -28,25 +48,32 @@ var travisPing = function(credentials, repo, cb) {
     if (err) {
       return cb(err);
     }
-    travis.repos.builds({
-      owner_name: repo.split('/')[0],
-      name: repo.split('/')[1]
-    }, function (err, res) {
+    
+    travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(function (err, res) {
       if (err) {
         return cb(err);
       }
       
-      travis.requests({
-        build_id: res.builds[0].id
-      }, cb);
+      findBuild(travis, res.builds, res.commits, filter, function(err, build) {
+        if (err) {
+          return cb(err);
+        }
+        
+        travis.requests.post({
+          build_id: build.id
+        }, function(err, res) {
+          if (!err && !res.result) err = res.flash[0];
+          cb(err, build);
+        });
+      });
     });
   });
 };
 
-exports.ping = function(credentials, repo, cb) {
+exports.ping = function(credentials, repo, filter, cb) {
   if (!credentials || !repo) throw "You need to specify your github credentials and the repo you would like to rebuild (eg patrickkettner/travis-ping)";
 
-  travisPing(credentials, repo, cb);
+  travisPing(credentials, repo, filter, cb);
 };
 
 exports.interpret = function(args, settings) {
@@ -61,12 +88,17 @@ exports.interpret = function(args, settings) {
   var ping = function(credentials) {
     credentials.pro = settings.pro;
     
-    travisPing(credentials, repo, function(err, res) {
+    filter = {};
+    if (settings.branch) filter.branch = settings.branch; 
+    if (settings.push && !settings.pullRequest) filter.eventType = 'push'; 
+    if (settings.pullRequest && !settings.push) filter.eventType = 'pull_request'; 
+    
+    travisPing(credentials, repo, filter, function(err, build) {
       if (err) {
         console.warn(err);
         process.exit(1);
       }
-      console.log(res.flash[0]);
+      console.log('Successfully restarted build #' + build.number);
     });
   };
   
